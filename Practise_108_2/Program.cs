c136fcca442367d63f225ce473651110dc670e7c
StenliH <hlavam12@gmail.com>
2018-10-30 11:29:01 +0100

    two new exercises added

---
+++ Practise_108_2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practise_108_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool goOn = true;
+            do
+            {
+                string[] listOfNames = new string[] { "Emil", "šmoula", "GASTro", "ho10Tot" };
+                Console.WriteLine("Zadejte hledané jméno:");
+                string name = Console.ReadLine();
+
+                Console.WriteLine(ContainsItem(listOfNames, name));
+
+                Console.WriteLine("Chcete pokračovat? (a/n)");
+                if (Console.ReadLine() == "n")
+                    goOn = false;
+                Console.Clear();
+
+            } while (goOn);
+        }
+
+        static string ContainsItem(string[] listOfNames, string name)
+        {
+            //string temp = "";
+
+            foreach (string item in listOfNames)
+            {
+                if (item.ToLower() == name.ToLower())
+                    return $"Nalezen uživatel jménem: {item}.";
+            }
+
+            return "Uživatel nenalezen.";
+        }
+    }
+}
---
+++ Practise_109_3/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practise_109_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zadejte rok narození: ");
+            int year = int.Parse(Console.ReadLine());
+            Console.Write("Zadejte měsíc narození: ");
+            int month = int.Parse(Console.ReadLine());
+            Console.Write("Zadejte den narození: ");
+            int day = int.Parse(Console.ReadLine());
+
+            DateTime dateOfBirth = new DateTime(year, month, day);
+
+            Console.WriteLine($"Máte {HowOldIs(dateOfBirth)} let.");
+
+            BirthdayWathDog(dateOfBirth);
+
+            Console.ReadKey();
+
+        }
+
+        static int HowOldIs(DateTime dateOfBirth)
+        {
+            /*
+            DateTime aktualniDatum = DateTime.Now.Date;
+
+
+            if (aktualniDatum.DayOfYear >= dateOfBirth.DayOfYear)
+            {
+                return aktualniDatum.Year - dateOfBirth.Year;
+            }
+            return aktualniDatum.Year - dateOfBirth.Year - 1;
+            */
+
+            DateTime aktualniDatum = new DateTime(2015, 8, 5);
+            return aktualniDatum.Year - dateOfBirth.Year;
+
+        }
+
+        static void BirthdayWathDog(DateTime dateOfBirth)
+        {
+            //DateTime aktualniDatum = DateTime.Now.Date;
+            DateTime aktualniDatum = new DateTime(2015, 8, 5);
+
+            int rozdilDnu = aktualniDatum.DayOfYear - dateOfBirth.DayOfYear;
+
+            if (rozdilDnu == 0)
+            {
+                Console.WriteLine("Happy birthday man!!");
+            }
+            else if (rozdilDnu < 0)
+            {
+                Console.WriteLine($"Do narozenin zbývá {-rozdilDnu} dnů.");
+            }
+            else
+            {
+                Console.WriteLine($"Uplynulo {rozdilDnu} dnů od narozenin.");
+            };
+        }
+    }
+}
